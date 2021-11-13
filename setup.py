ad9341cdd160cdf949a7d5291d4123f95b515f17
niklases <60261497+niklases@users.noreply.github.com>
2021-11-13 23:36:41 +0100

    Update setup.py 0.1.7

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 with open("requirements.txt", "r", encoding="utf-8") as install_requirements:
     requirements = install_requirements.read()
 
 setup(
     name='pypef',
-    version='0.1.6',
+    version='0.1.7',
     author='Niklas Siedhoff & Alexander-Maurice Illig',
     author_email='n.siedhoff@biotec.rwth-aachen.de',
     license='CC BY-NC-SA 4.0',
-    description='A command-line interface tool for performing data-driven protein engineering by building '
-                'machine learning models from sequence variant-fitness data (e.g., provided as CSV data).'
-                'Get the small API provided for encoding with: from pypef import encoding_api',
-    long_description=long_description,
+    description='A command-line interface (CLI) tool for performing data-driven protein engineering by building '
+                'machine learning models from sequence variant-fitness data (e.g., provided as CSV data). '
+                'Get a small API provided for encoding and model validation from modules in pypef.api.',
+    long_description='For detailed description including a short Jupyter Notebook-based '
+                     'tutorial please refer to the GitHub page.',
     long_description_content_type='text/markdown',
     url='https://github.com/Protein-Engineering-Framework/PyPEF',
     py_modules=['pypef'],
@@ -36,7 +35,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'pypef=pypef.run_pypef:run'
+            'pypef=pypef.cli.run:run_pypef'
         ],
     }
 )
