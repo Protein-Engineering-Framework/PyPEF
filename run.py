ced8bee98dcaa1a9f8556008da1366061369f7a3
niklases <60261497+niklases@users.noreply.github.com>
2023-08-24 09:57:02 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from pypef.main import run_main
+
+
+if __name__ == '__main__':
+    run_main()
